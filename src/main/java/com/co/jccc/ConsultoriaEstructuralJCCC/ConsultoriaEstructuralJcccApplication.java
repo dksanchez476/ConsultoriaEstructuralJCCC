36d05ae0e6e3f73218ba14ecb5d534ca0cbbc231
Katherin Sanchez <dksanchez476@misena.edu.co>
2021-07-12 22:28:30 -0500

    Initial commit

---
+++ src/main/java/com/co/jccc/ConsultoriaEstructuralJCCC/ConsultoriaEstructuralJcccApplication.java
@@ -0,0 +1,13 @@
+package com.co.jccc.ConsultoriaEstructuralJCCC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultoriaEstructuralJcccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultoriaEstructuralJcccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/jccc/ConsultoriaEstructuralJCCC/ConsultoriaEstructuralJcccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.jccc.ConsultoriaEstructuralJCCC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultoriaEstructuralJcccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
